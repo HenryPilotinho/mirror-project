afe9fe23833906d1eac292318e57c478348054f9
henrypilotinho <henriquepilotl@hotmail.com>
2023-05-26 08:15:21 -0300

    att

--- js/banner.js
+++ js/banner.js
@@ -3,6 +3,6 @@ var bannerAtual = 0;
 function trocaBanner()
 {
     bannerAtual = (bannerAtual + 1) % 2;
-    document.querySelector('.banner-destaque img').src = banners[bannerAtual];
+    document.querySelector('.banner-destaques img').src = banners[bannerAtual];
 }
 setInterval(trocaBanner, 3000);
